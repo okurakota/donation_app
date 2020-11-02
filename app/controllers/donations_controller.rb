30c575dd7b06ff375f058f6c7f4d28810f09a68c
okurakota <okura.kota01@gmail.com>
2020-11-02 15:57:32 +0900

    寄付アプリ完成

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,26 @@
+class DonationsController < ApplicationController
+  def index
+  end
+
+  def new
+    @user_donation = UserDonation.new   #「UserDonation」に編集
+  end
+
+  def create
+    @user_donation = UserDonation.new(donation_params)   #「UserDonation」に編集
+    if @user_donation.valid?
+      @user_donation.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+   # 全てのストロングパラメーターを1つに統合
+ def donation_params
+  params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
+ end
+ 
+end
